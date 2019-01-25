61a8bf2289f81435e8378b15ac834f2c3fff1966
GuoLei Sun <sunguolei@100tal.com>
2019-01-25 18:51:27 +0800

    support the node npm package

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-
 /**
  * Module dependencies.
  */
+'use strict';
 
-var express = require('express');
-var http = require('http');
-var path = require('path');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// app.use(express.logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+// app.use(express.urlencoded());
+// app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/tests')));
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// if ('development' == app.get('env')) {
+//     app.use(express.errorHandler());
+// }
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
